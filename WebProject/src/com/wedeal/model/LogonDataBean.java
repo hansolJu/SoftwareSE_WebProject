6a92a7861b2384a073a951297ed28f3ec59fbc80
컴퓨터 <@DESKTOP-C01FUL7>
2017-10-15 14:11:02 +0900

    전체 회원 맴버 관리 dto 추가

--- WebProject/src/com/wedeal/model/LogonDataBean.java
+++ WebProject/src/com/wedeal/model/LogonDataBean.java
@@ -4,9 +4,83 @@ import java.sql.Timestamp;
 
 public class LogonDataBean {
 
-	private String id;
-	private String passwd;
-	private String name;
-	private Timestamp reg_date; //���Գ�¥
-	private Timestamp last_date; //�ֱٹ湮��
+	private String id;//아이디
+	private String passwd;//비번
+	private String name;//이름
+	private Timestamp reg_date; //가입일
+	private Timestamp last_date; //최근방문일
+	private int visitCount;//방문수
+	private int postCount;//게시글수
+	private int commentCount;//댓글수
+	private String gender;//성별
+	private String ages;//나이
+	private boolean active;//활동정지
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Timestamp getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Timestamp reg_date) {
+		this.reg_date = reg_date;
+	}
+	public Timestamp getLast_date() {
+		return last_date;
+	}
+	public void setLast_date(Timestamp last_date) {
+		this.last_date = last_date;
+	}
+	public int getVisitCount() {
+		return visitCount;
+	}
+	public void setVisitCount(int visitCount) {
+		this.visitCount = visitCount;
+	}
+	public int getPostCount() {
+		return postCount;
+	}
+	public void setPostCount(int postCount) {
+		this.postCount = postCount;
+	}
+	public int getCommentCount() {
+		return commentCount;
+	}
+	public void setCommentCount(int commentCount) {
+		this.commentCount = commentCount;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getAges() {
+		return ages;
+	}
+	public void setAges(String ages) {
+		this.ages = ages;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
 }
