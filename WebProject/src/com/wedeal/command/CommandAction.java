8aed9b035571a37ff7fd91a70dd0b593bf48f6fa
DESKTOP-GTH902A\HansolJu <hansol1108@naver.com>
2017-11-02 13:32:42 +0900

    주석처리

--- WebProject/src/com/wedeal/command/CommandAction.java
+++ WebProject/src/com/wedeal/command/CommandAction.java
@@ -1,5 +1,8 @@
 package com.wedeal.command;
-
+/*
+ * CommandAction 는 쇼핑몰에서 명령어 처리 클래스의 진입점으로, 명령어 처리 클래스를 관리한다.
+ * CommandAction separates and manages shopping as a career point in the command class.
+ */
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
