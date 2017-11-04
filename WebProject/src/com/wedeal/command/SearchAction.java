4e64b217b9934cad10e0fa2d807af49d36a2f1b9
eunjin <eunjin@LAPTOP-4LQVBRJ5>
2017-11-05 02:24:27 +0900

    searchAction

--- WebProject/src/com/wedeal/command/SearchAction.java
+++ WebProject/src/com/wedeal/command/SearchAction.java
@@ -1,9 +1,33 @@
 package com.wedeal.command;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-abstract public class SearchAction {
-	List</*물품*/> productList = null;
-	List</*물품고유키*/> resultProductList = null;
+public class SearchAction implements CommandAction {
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		String searchName = null;
+		SearchService searchService = SearchService.getInstance();
+		ArrayList<ProductDTO> list;
+		try {
+			searchName = request.getParameter("searchName");
+		} catch(NullPointerException e) {
+			e.printStackTrace();
+			request.setAttribute("error_message", e.getMessage() +"값을 입력해주세요");
+			return "error";
+		}
+		try {
+			list = searchService.searchByName(searchName);
+			request.setAttribute("searchNameList", list);
+			request.setAttribute("search_value", searchName);
+		} catch(SQLException e) {
+			e.printStackTrace();
+			request.setAttribute("error_message",  e.getMessage());
+		}
+	}
 }
