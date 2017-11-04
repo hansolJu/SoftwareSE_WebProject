31223057040be7e2a96afba1045ab1230b088822
eunjin <eunjin@LAPTOP-4LQVBRJ5>
2017-11-05 03:46:29 +0900

    물품(게시글)dto 만들기 전 물품DAO

--- WebProject/src/com/wedeal/dao/ProductDAO.java
+++ WebProject/src/com/wedeal/dao/ProductDAO.java
@@ -6,30 +6,37 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import com.wedeal.model.ProductDataBean;
 import com.wedeal.util.DbUtil;
 
 public class ProductDAO {
 	
 	private Connection connection;
-	private static ProductDAO instance = new ProductDAO(); //싱글턴
+	private static ProductDataBean instance = new ProductDataBean(); //싱글턴
 
 	private ProductDAO() {
 		connection = DbUtil.getConnection();
 	}
-	private static ProductDAO getInstance() {
+	private static ProductDataBean getInstance() {
 		return instance;
 	}
-	public ArrayList<ProductDTO> selectProductName(String searchName) throws SQLException{
+	public ArrayList<ProductDataBean> selectProductName(String searchName) throws SQLException{
 		ResultSet rset = null;
 		
-		ArrayList<ProductDTO> searchNameProductList = new ArrayList<ProductDTO>();
-		PreparedStatement preparedStatement = connection.prepareStatement("select * from where product like '%'||?||'%'");
+		ArrayList<ProductDataBean> searchNameProductList = new ArrayList<ProductDataBean>();
+		PreparedStatement preparedStatement = connection.prepareStatement("select * from product where name like '%'||?||'%'");
 		preparedStatement.setString(1,  searchName);
 		rset = preparedStatement.executeQuery();
 		while(rset.next()) {
-			searchNameProductList.add(rset.get)
+			//각 dto 변수 저장
+			ProductDataBean product = new ProductDataBean();
+			product.setProductID(rset.getInt(0));
+			product.setProductName(rset.getString(1));
+			//더 추가해야함
+			searchNameProductList.add(product);
 		}
-		
+		DbUtil.close(connection, preparedStatement, rset);
+		return searchNameProductList;
 	}
 
 }
