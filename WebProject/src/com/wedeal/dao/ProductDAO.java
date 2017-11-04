ec9d79df62fed197be7bfce779cc03ad78991318
eunjin <eunjin@LAPTOP-4LQVBRJ5>
2017-11-05 04:26:05 +0900

    category별 물품

--- WebProject/src/com/wedeal/dao/ProductDAO.java
+++ WebProject/src/com/wedeal/dao/ProductDAO.java
@@ -15,27 +15,61 @@ public class ProductDAO {
 	private static ProductDAO instance = new ProductDAO(); //싱글톤
 
 	private ProductDAO() {
-		connection = DbUtil.getConnection();
 	}
 	public static ProductDAO getInstance() {
 		return instance;
 	}
-	public ArrayList<ProductDataBean> selectProductName(String searchName) throws SQLException{
+	/**
+	 * 
+	 * @param category
+	 * @return
+	 * @throws SQLException
+	 * 해당 카테고리의 물품들을 반환하는 메소드
+	 */
+	public ArrayList<ProductDataBean> selectProduct(String category) throws SQLException {
+		connection = DbUtil.getConnection();  //connection 연결
 		ResultSet rset = null;
 		
-		ArrayList<ProductDataBean> searchNameProductList = new ArrayList<ProductDataBean>();
-		PreparedStatement preparedStatement = connection.prepareStatement("select * from product where name like '%'||?||'%'");
-		preparedStatement.setString(1,  searchName);
+		ArrayList<ProductDataBean> searchCategoryProductList = new ArrayList<ProductDataBean>();  //반환할 결과 리스트
+		PreparedStatement preparedStatement = connection.prepareStatement("select * from product where name=?");  //해당 이름을 포함하는 물품 검색
+		preparedStatement.setString(1, category);
 		rset = preparedStatement.executeQuery();
 		while(rset.next()) {
 			//각 dto 변수 저장
 			ProductDataBean product = new ProductDataBean();
 			product.setProductID(rset.getInt(0));
 			product.setProductName(rset.getString(1));
-			//더 추가해야함
-			searchNameProductList.add(product);
+			//TO-DO : 더 추가해야함
+			searchCategoryProductList.add(product);  //리스트에 추가
 		}
-		DbUtil.close(connection, preparedStatement, rset);
+		DbUtil.close(connection, preparedStatement, rset);  //연결 해지
+		return searchCategoryProductList;
+	}
+	/**
+	 * 
+	 * @param searchName
+	 * @return 결과 list 반환
+	 * @throws SQLException
+	 * 이름으로 물품을 검색하는 메소드
+	 * TO-DO : dto에 맞게 변수 추가
+	 */
+	public ArrayList<ProductDataBean> selectProductByName(String searchName) throws SQLException{
+		connection = DbUtil.getConnection();  //connection 연결
+		ResultSet rset = null;
+		
+		ArrayList<ProductDataBean> searchNameProductList = new ArrayList<ProductDataBean>();  //반환할 결과 리스트
+		PreparedStatement preparedStatement = connection.prepareStatement("select * from product where name like '%'||?||'%'");  //해당 이름을 포함하는 물품 검색
+		preparedStatement.setString(1, searchName);
+		rset = preparedStatement.executeQuery();
+		while(rset.next()) {
+			//각 dto 변수 저장
+			ProductDataBean product = new ProductDataBean();
+			product.setProductID(rset.getInt(0));
+			product.setProductName(rset.getString(1));
+			//TO-DO : 더 추가해야함
+			searchNameProductList.add(product);  //리스트에 추가
+		}
+		DbUtil.close(connection, preparedStatement, rset);  //연결 해지
 		return searchNameProductList;
 	}
 
