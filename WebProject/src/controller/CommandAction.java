82715db48ea97712d3479f1d755990cbe7a95d91
Jihun <jihun8066@naver.com>
2017-11-09 08:43:33 +0900

    CommandAction 인터페이스 추가

---
+++ WebProject/src/controller/CommandAction.java
@@ -0,0 +1,10 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface CommandAction {
+	public String requestPro(
+		HttpServletRequest request, HttpServletResponse response)
+		throws Throwable;
+}
