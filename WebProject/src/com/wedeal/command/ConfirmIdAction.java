5ae5656472aafdf6cdde29a523414bda9ecbbb89
DESKTOP-GTH902A\HansolJu <hansol1108@naver.com>
2017-11-15 17:02:30 +0900

    중복확인 커멘드 (member/confrimId) 로 전송

--- WebProject/src/com/wedeal/command/ConfirmIdAction.java
+++ WebProject/src/com/wedeal/command/ConfirmIdAction.java
@@ -3,27 +3,6 @@ package com.wedeal.command;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-<<<<<<< HEAD
-import com.wedeal.bean.UserDBBean;
-
-public class ConfirmIdAction implements CommandAction{
-
-	@Override
-	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
-		request.setCharacterEncoding("utf-8");
-		String user_id = request.getParameter("user_id");
-		System.out.println(user_id);
-		System.out.println("�������~");
-		
-		UserDBBean manager = UserDBBean.getinstance();
-		int check = manager.registerCheck(user_id);
-		
-		request.setAttribute("check", new Integer(check));
-		return "/user/confirmId.jsp";
-	}
-	
-}
-=======
 import com.wedeal.bean.LogonDBBean;
 
 public class ConfirmIdAction implements CommandAction {
@@ -31,12 +10,9 @@ public class ConfirmIdAction implements CommandAction {
 	@Override
 	public String requestPro(HttpServletRequest request,
 			HttpServletResponse response) throws Throwable {
-		// TODO Auto-generated method stub
-		
 		request.setCharacterEncoding("utf-8");
 		String id = request.getParameter("id");
-		System.out.println("?��?��~");
-		//주어�? id?�� 중복?���?�? 체크?�� 값을 반환.
+		//주어진 id의 중복여부를 체크해 값을 반환.
 		LogonDBBean manager = LogonDBBean.getInstance();
 		int check= manager.confirmId(id);
 		
@@ -44,4 +20,3 @@ public class ConfirmIdAction implements CommandAction {
 		return "/member/confirmId.jsp";
 	}
 }
->>>>>>> master
