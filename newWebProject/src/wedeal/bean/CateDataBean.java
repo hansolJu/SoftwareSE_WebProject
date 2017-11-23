6e1170d2e674f43c76e466baedd81e822c796e66
컴퓨터 <@DESKTOP-C01FUL7>
2017-11-23 13:56:06 +0900

    .bean

---
+++ newWebProject/src/wedeal/bean/CateDataBean.java
@@ -0,0 +1,68 @@
+package wedeal.bean;
+/**
+ * 코드설명
+ * 작성자:이재윤
+ * 수정자: 17.11.20
+ * 최종수정일: 17.11.17
+ */
+public class CateDataBean {
+
+	private int cate_num;
+	private String cate_name;
+	private String upCategoryName;//상위 카테고리 이름
+	private String cate_date;//?? 이건 필요없을꺼 같은데
+	private String adminName;
+	private int cate_parent;//
+	private String savePath;
+	private String fileName;
+	public int getCate_num() {
+		return cate_num;
+	}
+	public void setCate_num(int cate_num) {
+		this.cate_num = cate_num;
+	}
+	public String getCate_name() {
+		return cate_name;
+	}
+	public void setCate_name(String cate_name) {
+		this.cate_name = cate_name;
+	}
+	public String getCate_date() {
+		return cate_date;
+	}
+	public void setCate_date(String cate_date) {
+		this.cate_date = cate_date;
+	}
+	public String getAdminName() {
+		return adminName;
+	}
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+	public int getCate_parent() {
+		return cate_parent;
+	}
+	public void setCate_parent(int cate_parent) {
+		this.cate_parent = cate_parent;
+	}
+	public String getSavePath() {
+		return savePath;
+	}
+	public void setSavePath(String savePath) {
+		this.savePath = savePath;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getUpCategoryName() {
+		return upCategoryName;
+	}
+	public void setUpCategoryName(String upCategoryName) {
+		this.upCategoryName = upCategoryName;
+	}
+	
+	
+}
---
+++ newWebProject/src/wedeal/bean/MngrDBBean.java
@@ -0,0 +1,97 @@
+package wedeal.bean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class MngrDBBean {
+	private Connection conn = null;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	private static MngrDBBean instance = new MngrDBBean();
+
+	public static MngrDBBean getinstance() {
+		return instance;
+	}
+
+	private MngrDBBean() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/se?autoReconnect=true&useSSL=false";
+			String dbID = "root";
+			String dbPW = "123123";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPW);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void deleteBoard(int cate_num) {
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement("delete from users where cate_num=?");
+            preparedStatement.setInt(1, cate_num);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+	}
+
+	public ArrayList<CateDataBean>  getAllBoard() {
+		String SQL="SELECT * FROM cate WHERE cate_parent > 0 ORDER BY cate_num DESC";
+		ArrayList<CateDataBean> list = new ArrayList<CateDataBean>();
+		try {
+			PreparedStatement pstmt=conn.prepareStatement(SQL);
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+				//여기도 테이블 몰라서 테이블에 맞게 추가해야대
+				CateDataBean catedatabean = new CateDataBean();
+				catedatabean.setCate_num(rs.getInt(1));
+				catedatabean.setCate_name(rs.getString(2));
+				catedatabean.setCate_date(rs.getString(3));
+				catedatabean.setCate_parent(rs.getInt(4));
+				list.add(catedatabean);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public CateDataBean getBoardById(int boardId) {
+		CateDataBean board = new CateDataBean();
+		try {
+			// 여기 테이블 뭔지 모르겠어!
+			PreparedStatement preparedStatement = conn.prepareStatement("select * from users where boardid=?");
+			preparedStatement.setInt(1, boardId);
+			ResultSet rs = preparedStatement.executeQuery();
+			if (rs.next()) {
+				board.setCate_num(rs.getInt("boardId"));
+				board.setUpCategoryName("upBoardName");
+				board.setCate_name("boardName");
+				board.setAdminName("adminName");
+				board.setSavePath("savePath");
+				board.setFileName("fileName");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return board;
+	}
+
+	public void addBoard(CateDataBean cateDataBean) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateBoard(CateDataBean cateDataBean) {
+		// TODO Auto-generated method stub
+		
+	}
+}
