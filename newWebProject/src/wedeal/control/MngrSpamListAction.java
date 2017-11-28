af28a01e91ee43ef5d06641e1141b6d22f1bbac5
컴퓨터 <@DESKTOP-C01FUL7>
2017-11-28 18:18:43 +0900

    .

---
+++ newWebProject/src/wedeal/control/MngrSpamListAction.java
@@ -0,0 +1,45 @@
+package wedeal.control;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MngrSpamListAction
+ */
+@WebServlet("/MngrSpamListAction")
+public class MngrSpamListAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MngrSpamListAction() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String forward = "";
+		String action = request.getParameter("action");
+		
+		if(action.equalsIgnoreCase("delete")) {//대소문자에 관계없이
+			
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
