db0413c184d67ae55fa127a7a2b7e0ba59d9ea31
jaeyo <jaeyo@192.168.0.90>
2017-11-28 17:24:01 +0900

    수정

--- newWebProject/src/wedeal/bean/UserDBBean.java
+++ newWebProject/src/wedeal/bean/UserDBBean.java
@@ -26,7 +26,7 @@ public class UserDBBean {
 
 	private UserDBBean() {
 		try {
-			String dbURL = "jdbc:mysql://204.249.22.34:3306/se?autoReconnect=true&useSSL=false";
+			String dbURL = "jdbc:mysql://203.249.22.34:3306/se?autoReconnect=true&useSSL=false";
 			String dbID = "jy";
 			String dbPW = "1365";
 			Class.forName("com.mysql.jdbc.Driver");
