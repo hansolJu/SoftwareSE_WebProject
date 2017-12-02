9a15c12ceb4429c587ed1e0b03df6c7ba9733a21
컴퓨터 <@DESKTOP-C01FUL7>
2017-12-02 18:52:09 +0900

    수정.

--- newWebProject/src/wedeal/control/CommentDeleteAction.java
+++ newWebProject/src/wedeal/control/CommentDeleteAction.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import wedeal.bean.*;
+import wedeal.bean.CommentDBBean;
 
 @WebServlet("/CommentDeleteAction")
 public class CommentDeleteAction extends HttpServlet {
