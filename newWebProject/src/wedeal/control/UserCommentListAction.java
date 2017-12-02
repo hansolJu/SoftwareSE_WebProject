559b80fb41965b12cb1f0fcd0002d62dbb3ba772
Jihun <jihun8066@naver.com>
2017-12-02 16:32:58 +0900

    새로 한거

---
+++ newWebProject/src/wedeal/control/UserCommentListAction.java
@@ -0,0 +1,47 @@
+package wedeal.control;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import wedeal.bean.CommentDBBean;
+import wedeal.bean.CommentDataBean;
+
+/**
+ * Servlet implementation class UserCommentList
+ */
+@WebServlet("/UserCommentList")
+public class UserCommentListAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String session_id = null;
+		ArrayList<CommentDataBean> commentlist = null;
+		
+		if(request.getSession().getAttribute("user_id")!=null)
+			session_id = (String)request.getSession().getAttribute("user_id");
+		
+		CommentDBBean comment = CommentDBBean.getinstance();
+		commentlist = comment.getUserCommentList(session_id);
+		
+		request.setAttribute("usercommentlist", commentlist);
+		request.getRequestDispatcher("user/myComment.jsp").forward(request, response);
+	}
+
+}
